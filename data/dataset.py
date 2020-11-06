269896044758b6f240d1bb279e24541963ce6f3b
chenwei <wei.chen@imotion.ai>
2020-11-06 23:36:03 +0800

    modity

--- data/dataset.py
+++ data/dataset.py
@@ -1,9 +1,18 @@
+# -*- coding: utf-8 -*-
+# --------------------------------------
+# @Time    : 2020/11/01
+# @Author  : Oscar Chen
+# @Email   : 530824679@qq.com
+# @File    : dataset.py
+# Description :preprocess data
+# --------------------------------------
+
 import cv2
 import os
 import numpy as np
 import tensorflow as tf
 from xml.etree import ElementTree as ET
-from cfg.config import path_params, model_params, solver_params, CLASS
+from cfg.config import path_params, model_params, solver_params, CLASSES
 
 class Dataset(object):
     def __init__(self):
@@ -14,16 +23,21 @@ class Dataset(object):
         self.image_size = model_params['image_size']
         self.cell_size = model_params['cell_size']
         self.class_num = model_params['num_classes']
-        self.class_ind = dict(zip(CLASS, range(self.class_num)))
+        self.class_ind = dict(zip(CLASSES, range(self.class_num)))
         self.batch_size = solver_params['batch_size']
         self.flipped = solver_params['flipped']
 
-    def image_read(self, image_num):
+    def load_image(self, image_num):
+        '''
+        依据image_num对相应的样本图片进行加载，同时执行resize操作，并对图像进行归一化操作
+        :param image_num: 图片编号
+        :return: 归一化后的图片数据
+        '''
         image_path = os.path.join(self.data_path, 'JPEGImages', image_num+'.jpg')
         image = cv2.imread(image_path)
 
-        self.h_ratio = self.image_size / image.shape[0]
-        self.w_ratio = self.image_size / image.shape[1]
+        self.h_ratio = 1.0 * self.image_size / image.shape[0]
+        self.w_ratio = 1.0 * self.image_size / image.shape[1]
 
         image = cv2.resize(image, (self.image_size, self.image_size), interpolation=cv2.INTER_LINEAR)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
@@ -33,7 +47,13 @@ class Dataset(object):
             return image, image[:, ::-1, :]
         return image
 
-    def make_label(self, image_num):
+    def load_annotation(self, image_num):
+        '''
+        对单个xml文件进行解析,输出为该图片样本对应的label矩阵，label为三维矩阵[cell_size, cell_size, 5 + 20],
+        confidence取值方法为:当前单元格包含目标则为１，不包含目标为０；(x, y, w, h)为box的形状信息，中心坐标，宽，高，均以像素坐标的形式给出
+        :param image_num: 图片编号
+        :return: 归一化后的图片数据
+        '''
         label = np.zeros([self.cell_size, self.cell_size, 5 + self.class_num], np.float32)
         label_path = os.path.join(self.data_path, 'Annotations', image_num+'.xml')
 
@@ -42,6 +62,7 @@ class Dataset(object):
 
         tree = ET.parse(label_path)
         root = tree.getroot()
+
         # 得到某个xml_file文件中所有的object
         objects = root.findall('object')
         for object in objects:
@@ -51,10 +72,13 @@ class Dataset(object):
             xmax = bndbox.find('xmax').text
             ymax = bndbox.find('ymax').text
 
+            # 将原始样本的标定转换为resize后的图片的标定,按照等比例转换的方式,从0开始索引
             x1 = max(min(float(xmin) * self.w_ratio, self.image_size - 1), 0)
             y1 = max(min(float(ymin) * self.h_ratio, self.image_size - 1), 0)
             x2 = max(min(float(xmax) * self.w_ratio, self.image_size - 1), 0)
             y2 = max(min(float(ymax) * self.h_ratio, self.image_size - 1), 0)
+
+            # 将类别由字符串转换为对应的int数
             class_index = self.class_ind[object.find('name').text.lower().strip()]
 
             center_x = (x1 + x2) / 2.0
@@ -62,13 +86,15 @@ class Dataset(object):
             width = x2 - x1
             height = y2 -y1
 
-            # 中心点相对于cell×cell网格的索引
+            # 计算当前目标属于第几个cell，从0开始索引
             center_x_index = int(center_x / self.image_size * self.cell_size)
             center_y_index = int(center_y / self.image_size * self.cell_size)
 
             # 对每个object,如果这个cell中有object了,则跳过标记
             if label[center_y_index, center_x_index, 0] == 1:
                 continue
+
+            # 这里的x, y坐标是交换的，原因在于numpy的索引和图像的索引是颠倒的，在图像中０维索引列，１维索引行
             label[center_y_index, center_x_index, 0] = 1
             label[center_y_index, center_x_index, 1:5] = [center_x, center_y, width, height]
             label[center_y_index, center_x_index, 5 + class_index] = 1
@@ -82,102 +108,3 @@ class Dataset(object):
             return label, label_flipped[:, ::-1, :]
         else:
             return label
-
-    # 定义函数转化变量类型,在将样本图片及标定数据写入tfrecord文件之前需要对两者的数据类型进行转换
-    def _int64_feature(self, value):
-        return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
-
-    # 数组形式的数据,首先转换为string,再转换为二进制形式进行保存
-    def _bytes_feature(self, value):
-        return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
-
-    def create_tfrecord(self):
-        # 获取作为训练验证集的图片序列
-        trainval_path = os.path.join(self.data_path, 'ImageSets', 'Main', 'trainval.txt')
-        if self.flipped:
-            tf_file = os.path.join(self.tfrecord_dir, self.train_tfrecord_name)
-            if not os.path.exists(tf_file):
-                # 循环写入每一张图像和标签到tfrecord文件
-                writer = tf.python_io.TFRecordWriter(tf_file)
-                with open(trainval_path, 'r') as read:
-                    lines = read.readlines()
-                    for line in lines:
-                        image_num = line[0:-1]
-
-                        # 获得当前样本数据和标签信息
-                        image, image_flipped = self.image_read(image_num=image_num)
-                        label, label_flipped = self.make_label(image_num=image_num)
-
-                        # 转换为字符串
-                        image_string = image.tostring()
-                        image_flipped_string = image_flipped.tostring()
-
-                        # 转换为字符串
-                        label_string = label.tostring()
-                        label_flipped_string = label_flipped.tostring()
-
-                        example = tf.train.Example(features=tf.train.Features(
-                            feature={
-                                'image': self._bytes_feature(image_string),
-                                'label': self._bytes_feature(label_string)}))
-                        writer.write(example.SerializeToString())
-                        example = tf.train.Example(features=tf.train.Features(
-                            feature={
-                                'image': self._bytes_feature(image_flipped_string),
-                                'label': self._bytes_feature(label_flipped_string)}))
-                        writer.write(example.SerializeToString())
-                writer.close()
-                print('Finish trainval.tfrecord Done')
-        else:
-            tf_file = os.path.join(self.tfrecord_dir, self.train_tfrecord_name)
-            if not os.path.exists(tf_file):
-                # 循环写入每一张图像和标签到tfrecord文件
-                writer = tf.python_io.TFRecordWriter(tf_file)
-                with open(trainval_path, 'r') as read:
-                    lines = read.readlines()
-                    for line in lines:
-                        image_num = line[0:-1]
-                        image = self.image_read(image_num)
-                        label = self.make_label(image_num)
-
-                        image_string = image.tostring()
-                        label_string = label.tostring()
-
-                        example = tf.train.Example(features=tf.train.Features(
-                            feature={
-                                'image': tf.train.Feature(bytes_list=tf.train.BytesList(value=[image_string])),
-                                'label': tf.train.Feature(bytes_list=tf.train.BytesList(value=[label_string]))
-                            }))
-                        writer.write(example.SerializeToString())
-                writer.close()
-                print('Finish trainval.tfrecord Done')
-
-
-    def load_tfrecord(self):
-
-        tf_file = os.path.join(self.tfrecord_dir, self.train_tfrecord_name)
-
-        filename_queue = tf.train.string_input_producer([tf_file])
-
-        reader = tf.TFRecordReader()
-
-        _, serialized_example = reader.read(filename_queue)
-
-        img_features = tf.parse_single_example(
-            serialized_example,
-            features={
-                'image': tf.FixedLenFeature([], tf.string),
-                'label': tf.FixedLenFeature([], tf.string)
-            })
-
-        image = tf.decode_raw(img_features['image'], tf.float32)
-
-        label = tf.decode_raw(img_features['label'], tf.float32)
-
-        image = tf.reshape(image, [self.image_size, self.image_size, 3])
-
-        label = tf.reshape(label, [self.cell_size, self.cell_size, 5 + self.class_num])
-
-        image_batch, label_batch = tf.train.shuffle_batch([image, label], batch_size=self.batch_size, capacity=2000, min_after_dequeue=100)
-
-        return image_batch, label_batch
---
+++ data/tfrecord.py
@@ -0,0 +1,180 @@
+# -*- coding: utf-8 -*-
+# --------------------------------------
+# @Time    : 2020/11/01
+# @Author  : Oscar Chen
+# @Email   : 530824679@qq.com
+# @File    : tfrecord.py
+# Description :create and parse tfrecord
+# --------------------------------------
+
+import os
+import numpy as np
+import tensorflow as tf
+from data.dataset import Dataset
+from cfg.config import path_params, model_params, solver_params, CLASSES
+
+class TFRecord(object):
+    def __init__(self):
+        self.data_path = path_params['data_path']
+        self.tfrecord_dir = path_params['tfrecord_dir']
+        self.train_tfrecord_name = path_params['train_tfrecord_name']
+        self.test_tfrecord_name = path_params['test_tfrecord_name']
+        self.image_size = model_params['image_size']
+        self.cell_size = model_params['cell_size']
+        self.class_num = model_params['num_classes']
+        self.class_ind = dict(zip(CLASSES, range(self.class_num)))
+        self.batch_size = solver_params['batch_size']
+        self.flipped = solver_params['flipped']
+        self.dataset = Dataset()
+
+    # 数值形式的数据,首先转换为string,再转换为int形式进行保存
+    def _int64_feature(self, value):
+        return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+    # 数组形式的数据,首先转换为string,再转换为二进制形式进行保存
+    def _bytes_feature(self, value):
+        return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+    def create_tfrecord(self):
+        # 获取作为训练验证集的图片序列
+        trainval_path = os.path.join(self.data_path, 'ImageSets', 'Main', 'trainval.txt')
+        if self.flipped:
+            tf_file = os.path.join(self.tfrecord_dir, self.train_tfrecord_name)
+            if not os.path.exists(tf_file):
+                # 循环写入每一张图像和标签到tfrecord文件
+                writer = tf.python_io.TFRecordWriter(tf_file)
+                with open(trainval_path, 'r') as read:
+                    lines = read.readlines()
+                    for line in lines:
+                        image_num = line[0:-1]
+
+                        # 获得当前样本数据和标签信息
+                        image, image_flipped = self.dataset.load_image(image_num=image_num)
+                        label, label_flipped = self.dataset.load_annotation(image_num=image_num)
+
+                        # 转换为字符串
+                        image_string = image.tostring()
+                        image_flipped_string = image_flipped.tostring()
+
+                        # 转换为字符串
+                        label_string = label.tostring()
+                        label_flipped_string = label_flipped.tostring()
+
+                        example = tf.train.Example(features=tf.train.Features(
+                            feature={
+                                'image': self._bytes_feature(image_string),
+                                'label': self._bytes_feature(label_string)}))
+                        writer.write(example.SerializeToString())
+                        example = tf.train.Example(features=tf.train.Features(
+                            feature={
+                                'image': self._bytes_feature(image_flipped_string),
+                                'label': self._bytes_feature(label_flipped_string)}))
+                        writer.write(example.SerializeToString())
+                writer.close()
+                print('Finish trainval.tfrecord Done')
+        else:
+            tf_file = os.path.join(self.tfrecord_dir, self.train_tfrecord_name)
+            if not os.path.exists(tf_file):
+                # 循环写入每一张图像和标签到tfrecord文件
+                writer = tf.python_io.TFRecordWriter(tf_file)
+                with open(trainval_path, 'r') as read:
+                    lines = read.readlines()
+                    for line in lines:
+                        image_num = line[0:-1]
+                        image = self.dataset.load_image(image_num)
+                        label = self.dataset.load_annotation(image_num)
+
+                        image_string = image.tostring()
+                        label_string = label.tostring()
+
+                        example = tf.train.Example(features=tf.train.Features(
+                            feature={
+                                'image': tf.train.Feature(bytes_list=tf.train.BytesList(value=[image_string])),
+                                'label': tf.train.Feature(bytes_list=tf.train.BytesList(value=[label_string]))
+                            }))
+                        writer.write(example.SerializeToString())
+                writer.close()
+                print('Finish trainval.tfrecord Done')
+
+    def parse_single_example(self, file_name):
+        """
+        :param file_name:待解析的tfrecord文件的名称
+        :return: 从文件中解析出的单个样本的相关特征，image, label
+        """
+
+        tfrecord_file = os.path.join(self.tfrecord_dir, self.train_tfrecord_name)
+
+        # 定义解析TFRecord文件操作
+        reader = tf.TFRecordReader()
+
+        # 创建样本文件名称队列
+        filename_queue = tf.train.string_input_producer([tfrecord_file])
+
+        # 解析单个样本文件
+        _, serialized_example = reader.read(filename_queue)
+        features = tf.parse_single_example(
+            serialized_example,
+            features={
+                'image': tf.FixedLenFeature([], tf.string),
+                'label': tf.FixedLenFeature([], tf.string)
+            })
+
+
+        image = features['image']
+        label = features['label']
+
+        return image, label
+
+    def parse_batch_examples(self, file_name):
+        """
+        :param file_name:待解析的tfrecord文件的名称
+        :return: 解析得到的batch_size个样本
+        """
+        batch_size = self.batch_size
+        min_after_dequeue = 100
+        num_threads = 3
+        capacity = min_after_dequeue + 3 * batch_size
+
+
+        image, label = self.parse_single_example(file_name)
+        image_batch, label_batch = tf.train.shuffle_batch([image, label],
+                                                          batch_size=batch_size,
+                                                          num_threads=num_threads,
+                                                          capacity=capacity,
+                                                          min_after_dequeue=min_after_dequeue)
+
+        # 进行解码
+        image_batch = tf.decode_raw(image_batch, tf.float32)
+        label_batch = tf.decode_raw(label_batch, tf.float32)
+
+        # 转换为网络输入所要求的形状
+        image_batch = tf.reshape(image_batch, [self.batch_size, self.image_size, self.image_size, 3])
+        label_batch = tf.reshape(label_batch, [self.batch_size, self.cell_size, self.cell_size, 5 + self.class_num])
+
+        return image_batch, label_batch
+
+if __name__ == '__main__':
+    file = 'pascal_voc_train.tfrecords'
+    tfrecord = TFRecord()
+    batch_example, batch_label = tfrecord.parse_batch_examples(file)
+    with tf.Session() as sess:
+
+        init_op = tf.global_variables_initializer()
+        sess.run(init_op)
+
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(coord=coord)
+        for i in range(1):
+            example, label = sess.run([batch_example, batch_label])
+            print(label)
+            print(label.astype(np.float32))
+            box = label[0, ]
+            # cv2.imshow('w', example[0, :, :, :])
+            # cv2.waitKey(0)
+            print(np.shape(example), np.shape(label))
+        # cv2.imshow('img', example)
+        # cv2.waitKey(0)
+        # print(type(example))
+        coord.request_stop()
+        # coord.clear_stop()
+        coord.join(threads)
